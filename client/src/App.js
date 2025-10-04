ebaf527848fafef7d6e174f835ddfd844a783233
Peehu Mishra <peehumishra1308@gmail.com>
2025-10-04 12:48:40 +0530

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import Box from './components/box_plants';
 import './index.css'
 import Box_grid from './components/box_grid';
 import { Plants_screen } from './components/plants_screen';
+import VegetationData from './components/vegetationdata';
+import LowVegetationData from './components/low_vegetation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Plants_screen/>
+        {/* <Plants_screen/> */}
+        <VegetationData/>
+        {/* <LowVegetationData/> */}
+
       </header>
     </div>
   );
